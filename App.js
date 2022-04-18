ff3481b0e46859fb814a7e441f68fae4cd046d2b
Victoria <vi.park6@gmail.com>
2022-04-18 18:32:24 +0200

    add item component

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Item from './components/Item';
+import React, { useState } from 'react';
+
+const mood = [
+  {
+    state: 'angry',
+    src: './angry.jpg'
+  },
+  {
+    state: 'happy',
+    src: './happy.jpg'
+  },
+  {
+    state: 'neutral',
+    src: './neutral.jpg'
+  },
+  {
+    state: 'sad',
+    src: './sad.jpg'
+  },
+  {
+    state: 'upset',
+    src: './upset.jpg'
+  }
+]
 
 export default function App() {
+  const [start, setStart] = useState(false);
+  const [moodIcon, setmoodIcon] = useState('');
+
   return (
-    <>
+    <View style={styles.container}>
       <Text style={styles.title}>Gratitude app 😇</Text>
-      <View style={styles.container}>
-        <Text>playing around with react native xD </Text>
-        <Image source={require('./happy.jpg')} style={{ height: 150, width: 150, resizeMode: 'contain', borderRadius: 20 }} />
-        <StatusBar style="auto" />
+      <Text style={styles.paragraph}>Count your blessings!</Text>
+
+      {!start && <Image source={require('./hero-icon.jpg')} style={{ height: 100, width: 100, resizeMode: 'contain' }} />}
+
+      <View style={styles.items}>
+        <Item />
+        <Item />
+        <Item />
       </View>
-    </>
+    </View>
   );
 }
 
@@ -18,13 +49,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
-    fontSize: 20,
+    fontSize: 26,
     fontWeight: 'bold',
-    textAlign: 'center',
     marginTop: 30,
+    textAlign: 'center'
+  },
+  paragraph: {
+    margin: 5,
+    textAlign: 'center',
+    fontStyle: 'italic'
+  },
+  items: {
+
   }
 });
---
+++ components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Item() {
+  return (
+    <View>
+      <Text>Grateful for ...</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+});
+
