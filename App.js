8409891a57a948fc9907ee392045bfeef0b0e0cc
Victoria <vi.park6@gmail.com>
2022-04-18 17:37:31 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text style={styles.title}>Gratitude app 😇</Text>
+      <View style={styles.container}>
+        <Text>playing around with react native xD </Text>
+        <Image source={require('./happy.jpg')} style={{ height: 150, width: 150, resizeMode: 'contain', borderRadius: 20 }} />
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 30,
+  }
 });
