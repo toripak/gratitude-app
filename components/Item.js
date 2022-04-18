0c2fe37d35bfea3ba497c0df6bf16ad875eef020
Victoria <vi.park6@gmail.com>
2022-04-18 23:50:58 +0200

    style Item component

--- components/Item.js
+++ components/Item.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Item() {
+export default function Item({ text }) {
   return (
-    <View>
-      <Text>Grateful for ...</Text>
+    <View style={styles.item}>
+      <Text>{text}</Text>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-
+  item: {
+    backgroundColor: '#FFF5F7',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+  }
 });
 
